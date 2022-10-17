d1e6fecfbf37a0e8ac60cdba0da0558cd4d11e15
Owie963 <32614482+Owie963@users.noreply.github.com>
2022-10-17 23:18:02 +0800

    copy master to branch

---
+++ src/Model/User.java
@@ -0,0 +1,14 @@
+package Model;
+/**
+ Represents a user in the MOBLIMA Cinema Application
+ @author CS2002 Group 
+ @version 1.0
+ @since 17-10-2022
+ */
+
+
+public class User {
+
+}
+
+
--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    int var = 102;
-    int testing = 12912;
-    int addingonyc = 999;
-    int myNum = 98266656;
-    int saldlas = 12312;
-}
