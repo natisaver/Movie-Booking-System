cefc202c430a2f574416ad2f38cf46648683cfb6
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2022-10-17 23:15:11 +0800

    test

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,7 +1,14 @@
-public static void main(String[] args) {
-    Sale simple = new Sale();
-    DiscountSale discount = new DiscountSale();
-    // if(!simple.lessThan(discount)){
-    //     System.out.println("code cmi");
-    // }
+package Model;
+/**
+ Represents a user in the MOBLIMA Cinema Application
+ @author CS2002 Group 
+ @version 1.0
+ @since 17-10-2022
+ */
+
+
+public class User {
+
 }
+
+
