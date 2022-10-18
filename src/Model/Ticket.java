f9dae5fa10fe7e14bcb2a0052335532badfc9bf6
yanchi <yanchi97@gmail.com>
2022-10-18 14:23:12 +0800

    Added Ticket Class

---
+++ src/Model/Ticket.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+ * Represents a ticket in the MOBLIMA Cinema Application
+ * @author CS2002 Group 
+ * @version 1.0
+ * @since 18-10-2022
+ */
+
+public class Ticket 
+{
+    
+    
+}
