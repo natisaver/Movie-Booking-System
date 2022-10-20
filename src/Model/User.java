4efeeb50b106c066f669adb8f0a3e89788a4b0c0
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2022-10-21 07:53:28 +0800

    completed User Class

--- src/Model/User.java
+++ src/Model/User.java
@@ -8,6 +8,86 @@ package Model;
 
 
 public class User {
+    /**
+	 * User's Name
+	 */
+	private String name;
+
+    /**
+	 * User's Email Address
+	 */
+    private String email;
+	
+	/**
+	 * User's Role (Admin, MovieGoer etc.)
+	 */
+	private int role; 
+	
+	/**
+     * Current Available Roles
+     */
+    public static final int MOVIE_GOER = 0, ADMIN = 1;
+    
+	/** 
+	 * Constructor
+     * @param name		    The User's name 
+	 * @param email			The User's email/username
+	 * @param role			The User's role 
+	 */
+	public User (String name, String email, int role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    /** 
+	 * Get the name of this User
+	 * @return String   this user's name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+    /** 
+	 * Get the email of this User
+	 * @return String   this user's email
+	 */
+	public String getEmail() {
+    	return this.email;
+	}
+    
+	/** Get the role of this User
+	 * @return int   this User's role
+	 */
+	public int getRole() {
+    	return this.role;
+	}
+
+    /**
+     * Set user's name
+     * @param name   user's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Set user's email
+     * @param email   user's email
+     */
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    /**
+     * Set user's role
+     * 0: MOVIE_GOER
+     * 1: ADMIN
+     * @param role   user's role
+     */
+    public void setRole(int role) {
+        this.role = role;
+    }
 
 }
 
