0c2b5b4bd52bc88ea3533efdef652a2906cbb0c4
yanchi <yanchi97@gmail.com>
2022-10-21 02:48:11 +0800

    Changed movieType_Enum element names. Updated setCast and getCast arguments and return type.

--- src/Model/Movie.java
+++ src/Model/Movie.java
@@ -1,5 +1,12 @@
 package Model;
 
+/**
+ * Represents a movie in the MOBLIMA Cinema Application
+ * @author CS2002 Group 
+ * @version 1.0
+ * @since 21-10-2022
+ */
+
 public class Movie {
     private String title;
     private String director;
@@ -15,128 +22,200 @@ public class Movie {
     private float reviewerRating;
     private int ticketSales;
 
+    /**
+     * This is the constructor of a Movie object. 
+     * 
+     * @param title Movie title
+     * @param director Movie director
+     * @param cast Movie cast (at least 2)
+     * @param releaseDate Movie release date
+     * @param endDate Movie ending date
+     * @param synopsis Movie synopsis
+     * @param duration Movie runtime
+     * @param showingStatus Movie showing status (Now showing, Coming soon or Preview)
+     * @param movieType Movie type (2D, 3D or Blockbuster)
+     * @param movieRating Movie rating (PG, PG13, NC16, M18 or R21)
+     */
+    public Movie(String title, String director, String[] cast, Date releaseDate, Date endDate, String synopsis,
+            int duration, showingStatus_Enum showingStatus, movieType_Enum movieType, movieRating_Enum movieRating) {
+        this.title = title;
+        this.director = director;
+        this.cast = cast;
+        this.releaseDate = releaseDate;
+        this.endDate = endDate;
+        this.synopsis = synopsis;
+        this.duration = duration;
+        this.showingStatus = showingStatus;
+        this.movieType = movieType;
+        this.movieRating = movieRating;
+    }
+
+    /**
+     * Get the title of the movie
+     * @return a String data type that contains the title of Movie object.
+     */
     public String getTitle() {
         return this.title;
     }
 
     /**
      * 
-     * @param title
+     * @param title Specify the title of the Movie object.
      */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get the director of the movie
+     * @return a String data type that contains the director of Movie object.
+     */
     public String getDirector() {
         return this.director;
     }
 
     /**
      * 
-     * @param director
+     * @param director Specify the director's name of the Movie object.
      */
     public void setDirector(String director) {
         this.director = director;
     }
-
-    public String getCast() {
-        // TODO - implement Movie.getCast
-        throw new UnsupportedOperationException();
+    /**
+     * Get an array of the casts of the movie
+     * @return a String array data type that contains the casts of Movie object.
+     */
+    public String[] getCast() {
+        return this.cast;
     }
 
     /**
      * 
-     * @param cast
+     * @param cast Specify an array of Movie object's cast.
      */
-    public void setCast(String cast) {
-        // TODO - implement Movie.setCast
-        throw new UnsupportedOperationException();
+    public void setCast(String[] cast) {
+        this.cast = cast;
     }
 
+    /**
+     * Get the release date of the movie
+     * @return a Date data type that contains the release date of Movie object.
+     */
     public Date getReleaseDate() {
         return this.releaseDate;
     }
 
     /**
      * 
-     * @param date
+     * @param date Specify the release date of the Movie object.
      */
     public void setReleaseDate(Date date) {
         this.releaseDate = date;
     }
 
+    /**
+     * Get the ending date of the movie
+     * @return a Date data type that contains the ending date of Movie object.
+     */
     public Date getEndDate() {
         return this.endDate;
     }
 
     /**
      * 
-     * @param date
+     * @param date Specify the ending date of the Movie object.
      */
     public void setEndDate(Date date) {
         this.endDate = date;
     }
 
+    /**
+     * Get the synopsis of the movie
+     * @return a String data type that contains the synopsis of Movie object.
+     */
     public String getSynopsis() {
         return this.synopsis;
     }
 
     /**
      * 
-     * @param synopsis
+     * @param synopsis Specify the synopsis of the Movie object. 
      */
     public void setSynopsis(String synopsis) {
         this.synopsis = synopsis;
     }
 
+    /**
+     * Get the runtime of the movie
+     * @return a int data type that contains the runtime of Movie object.
+     */
     public int getDuration() {
         return this.duration;
     }
 
     /**
      * 
-     * @param duration
+     * @param duration Specify the runtime of the Movie object.
      */
     public void setDuration(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * Get the showing status of the movie
+     * @return an Enum data type that contains the showing status of Movie object.
+     * Possible showing status: (COMING_SOON, NOW_SHOWING, PREVIEW, END_OF_SHOWING)
+     */
     public showingStatus_Enum getShowingStatus() {
         return this.showingStatus;
     }
 
     /**
      * 
-     * @param showingStatus
+     * @param showingStatus Specify showing status of Movie object. 
+     * Possible showing status: (COMING_SOON, NOW_SHOWING, PREVIEW, END_OF_SHOWING)
      */
     public void setShowingStatus(showingStatus_Enum showingStatus) {
         this.showingStatus = showingStatus;
     }
 
+    /**
+     * Get the movie type of the movie
+     * @return a Enum data type that contains the movie type of Movie object.
+     * Possible movie type: (TWOD, THREED, BLOCKBUSTER)
+     */    
     public movieType_Enum getMovieType() {
         return this.movieType;
     }
 
     /**
      * 
-     * @param movieType
+     * @param movieType Specify movie type of Movie object.
+     * Possible movie type: (TWOD, THREED, BLOCKBUSTER)
      */
     public void setMovieType(movieType_Enum movieType) {
         this.movieType = movieType;
     }
 
+    /**
+     * Get the movie rating of the movie
+     * @return a Enum data type that contains the movie rating of Movie object.
+     * Possible movie rating: (PG, PG13, NC16, M18, R21)
+     */    
     public movieRating_Enum getMovieRating() {
         return this.movieRating;
     }
 
     /**
      * 
-     * @param movieRating
+     * @param movieRating Specify movie rating of Movie object
+     * Possible movie rating: (PG, PG13, NC16, M18, R21)
      */
     public void setMovieRating(movieRating_Enum movieRating) {
         this.movieRating = movieRating;
     }
 
+    
     public HashMap<MovieGoer, Review> getReviewList() {
         return this.reviewList;
     }
@@ -164,24 +243,4 @@ public class Movie {
         // TODO - implement Movie.addTicketSales
         throw new UnsupportedOperationException();
     }
-
-    /**
-     * 
-     * @param title
-     * @param director
-     * @param cast
-     * @param releaseDate
-     * @param endDate
-     * @param synopsis
-     * @param duration
-     * @param showingStatus
-     * @param movieType
-     * @param movieRating
-     */
-    public Movie(String title, String director, String[] cast, Date releaseDate, Date endDate, String synopsis,
-            int duration, showingStatus_Enum showingStatus, movieType_Enum movieType, movieRating_Enum movieRating) {
-        // TODO - implement Movie.Movie
-        throw new UnsupportedOperationException();
-    }
-
 }
