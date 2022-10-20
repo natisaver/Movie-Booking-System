b8ceff56c1e455edd6464d2e5b1ec05267b43385
Owie963 <32614482+Owie963@users.noreply.github.com>
2022-10-21 02:17:15 +0800

    modified MovieSession and Seat

--- src/Model/MovieSession.java
+++ src/Model/MovieSession.java
@@ -1,9 +1,23 @@
 package Model;
 
+import java.time.LocalTime;
+
 public class MovieSession {
     private LocalTime showtime;
-    private Seat[] sessionSeats;
-    private Movie movie;
+    private Seat[][] sessionSeats;
+    private String movieTitle;
+
+    public MovieSession(LocalTime time, int maxRow, int maxCol, String title) {
+        this.showtime = time;
+        this.sessionSeats = new Seat[maxRow][maxCol];
+        for (int i=0;i<maxRow;i++) {
+            for (int j=0;j<maxCol;j++) {
+                String id = "" + (char)(i+'A') + String.valueOf(j+1);
+                sessionSeats[i][j] = new Seat(id);
+            }
+        }
+        this.movieTitle = title;
+    }
 
     public LocalTime getShowtime() {
         return this.showtime;
@@ -15,6 +29,10 @@ public class MovieSession {
      */
     public void setShowtime(LocalTime time) {
         this.showtime = time;
+    }
+
+    public String getTitle() {
+        return this.movieTitle;
     }
 
 }
--- src/Model/Seat.java
+++ src/Model/Seat.java
@@ -7,4 +7,32 @@ public class Seat {
     private int seatCol;
     private boolean isOccupied;
 
+    public Seat(String id) {
+        this.seatID = id;
+        char row = id.charAt(0);
+        this.seatRow = ((int)row - 65);
+        char col = id.charAt(1);
+        this.seatCol = ((int)col - 49);
+        isOccupied = false;
+    }
+
+    public String getSeatID() {
+        return this.seatID;
+    }
+
+    public int getSeatRow() {
+        return this.seatRow;
+    }
+
+    public int getSeatCol() {
+        return this.seatCol;
+    }
+
+    public boolean getIsOccupied() {
+        return this.isOccupied;
+    }
+
+    public void setIsOccupied() {
+        this.isOccupied = !this.isOccupied;
+    }
 }
