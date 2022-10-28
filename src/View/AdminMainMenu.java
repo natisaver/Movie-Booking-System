1739f93f27bdd80bebd49ed344d1fa976ccfc9ba
nicoleveronica <nicoleveronica.su@gmail.com>
2022-10-29 00:18:05 +0800

    menu edited 29 oct

--- src/View/AdminMainMenu.java
+++ src/View/AdminMainMenu.java
@@ -10,37 +10,42 @@ public class AdminMainMenu extends BaseMenu{
 
     @Override
     public BaseMenu execute() {
+        int choice;
+
         System.out.println("Admin Menu Options:");
         System.out.println("1. Configure System Settings");
         System.out.println("2. Enter movie details");
         System.out.println("3. Update movie details");
         System.out.println("4. List Top 5 movies");
         System.out.println("5. Back");
+        do{
+            System.out.print("Enter your choice:");
+            choice = sc.nextInt();
 
-        System.out.print("Enter your choice:");
-        int choice = sc.nextInt();
+            BaseMenu nextMenu = this;
+            switch (choice) {
+                case 1:
+                    nextMenu = new ConfigureSettings(this);
+                    break;
+                case 2:
+                    nextMenu = new EnterDetails(this);
+                    break;
+                case 3:
+                    nextMenu = new UpdateDetails(this);
+                    break;
+                case 4:
+                    nextMenu = new ListTop5(this);
+                    break;
+                case 5:
+                    nextMenu = this.getPreviousMenu();
+                    break;
+                default:
+                    choice = -1;
+                    System.out.println("Please enter a valid choice.");
+                    break;
+            }
+        }while(choice == -1);
 
-        BaseMenu nextMenu = this;
-        switch (choice) {
-            case 1:
-                nextMenu = new ConfigureSettings(this);
-                break;
-            case 2:
-                nextMenu = new EnterDetails(this);
-                break;
-            case 3:
-                nextMenu = new UpdateDetails(this);
-                break;
-            case 4:
-                nextMenu = new ListTop5(this);
-                break;
-            case 5:
-                nextMenu = this.getPreviousMenu();
-                break;
-            default:
-                System.out.println("Please enter a valid choice.");
-                break;
-        }
         return nextMenu;
 
     }
