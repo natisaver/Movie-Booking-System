de3d93a72f792a27007b971bafb7a7e2218781a1
Owie963 <32614482+Owie963@users.noreply.github.com>
2022-11-03 21:18:10 +0800

    Updated booking functions

--- src/Model/MovieSession.java
+++ src/Model/MovieSession.java
@@ -109,17 +109,51 @@ public class MovieSession {
     }
 
     /**
-     * Book a seat in this MovieSession
-     * @param id
+     * Books a seat in that MovieSession
+     * @param id            The seat ID that the MovieGoer inputs
+     * @param cinemaClass   The cinemaClass of the cinema that the MovieSession is being held in
+     * @return              <code>true</code> if the booking was successful, otherwise returns <code>false</code>
      */
-    public void bookSeat(String id) {
+    public boolean bookSeat(String id, cinemaClass_Enum cinemaClass) {
+        if (cinemaClass == cinemaClass_Enum.STANDARD) {
+            if (id.equals("K5")) {
+                System.out.println("Invalid seat! Please choose again");
+                return false;
+            }
+        }
+        if (cinemaClass == cinemaClass_Enum.MAX) {
+            if ((id.equals("S1")) || (id.equals("S2")) || (id.equals("S35")) || (id.equals("S36"))) {
+                System.out.println("Invalid seat! Please choose again");
+                return false;
+            }
+        }
 		int row, column;
+        if (cinemaClass == cinemaClass_Enum.STANDARD) {
+            if (Character.compare(id.charAt(0), 'K') == 0) {
+				row = (int)(id.charAt(0) - 66);
+				if (id.length() > 2) column = Integer.parseInt(id.substring(id.length()-2))-1;
+				else column = Integer.parseInt(id.substring(1))-1;
+				if (column >= 5) column -= 1;
+				this.sessionSeats[row][column].setIsOccupied();
+				return true;
+			}
+		}
+        if (cinemaClass == cinemaClass_Enum.MAX) {
+			if (Character.compare(id.charAt(0), 'S') == 0) {
+				row = (int)(id.charAt(0) - 67);
+				if (id.length() > 2) column = Integer.parseInt(id.substring(id.length()-2))-3;
+				else column = Integer.parseInt(id.substring(1))-3;
+				this.sessionSeats[row][column].setIsOccupied();
+				return true;
+			}
+		}
 		if (Character.compare('O', id.charAt(0)) < 0) row = (int)(id.charAt(0) - 67);
 		else if (Character.compare('I', id.charAt(0)) < 0) row = (int)(id.charAt(0) - 66);
 		else row = (int)(id.charAt(0) - 65);
 		if (id.length() > 2) column = Integer.parseInt(id.substring(id.length()-2))-1;
 		else column = Integer.parseInt(id.substring(1))-1;
 		this.sessionSeats[row][column].setIsOccupied();
+        return true;
 	}
 
     /**
