6289bb5d6febcb90cab8d8026518bd3c9f83f691
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2022-11-05 17:37:07 +0800

    fixed quotes in password

--- src/View/CreateAccount.java
+++ src/View/CreateAccount.java
@@ -57,11 +57,19 @@ public class CreateAccount extends BaseMenu {
         System.out.println(ConsoleColours.GREEN + "(Leave any field empty to quit)" + ConsoleColours.RESET);
 
         do {
-            System.out.println("Your Name: ");
-            name = sc.nextLine();
-            if(name.isBlank()){
-                break;
-            }
+            do {
+                System.out.println("Your Name: ");
+                name = sc.nextLine();
+                if(name.isBlank()){
+                    return this.getPreviousMenu();
+                }
+                regexPattern = Pattern.compile("^[^,]+$");
+                regMatcher = regexPattern.matcher(name);
+                if (regMatcher.matches()){
+                    break;
+                }
+            } while (!regMatcher.matches());
+
             System.out.println("ur indicated name is " + name);
 
             System.out.println("Email: ");
