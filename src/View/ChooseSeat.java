6dbb1a4413cd2a11ccc974d458c2668144a54e07
Owie963 <32614482+Owie963@users.noreply.github.com>
2022-11-08 21:27:30 +0800

    Added ChooseSeat.java

---
+++ src/View/ChooseSeat.java
@@ -0,0 +1,15 @@
+package View;
+
+import java.util.Scanner;
+
+import Model.MovieSession;
+import Controller.MovieSessionController;
+
+public class ChooseSeat extends BaseMenu {
+  Scanner sc = new Scanner(System.in);
+
+  @Override
+  public BaseMenu execute() {
+
+  }
+}
